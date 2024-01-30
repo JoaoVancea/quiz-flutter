542a1f3761d6e4b628fbb61e096d70a281b7681d
João Carlos <joaocdvancea@gmail.com>
2024-01-30 11:03:30 -0300

    página de quiz terminado concluída

---
+++ lib/quizMarinho.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:projeto_quiz/home_page.dart';
+
+class QuizMarinho extends StatefulWidget {
+  const QuizMarinho({Key? key}) : super(key: key);
+
+  @override
+  State<QuizMarinho> createState() => _QuizState();
+}
+
+class _QuizState extends State<QuizMarinho> {
+  int currentQuestionIndex = 0;
+  int questoesAcertadas = 0;
+  String? selectedAnswer;
+  bool? isCorret;
+  bool quizConcluido = false;
+
+  final List<Map<String, dynamic>> questions = [
+    {
+      'question': 'Qual destes animais é conhecido por sua impressionante capacidade de mudar de cor?',
+      'answers': ['Golfinho', 'Polvo', 'Tubarão', 'Baleia'],
+      'correctAnswer': 'Polvo'
+    },
+    {
+      'question': 
+        'Qual animal marinho é famoso por sua carapaça dura e longa vida?',
+      'answers': [
+        'Baleia Azul',
+        'Caranguejo',
+        'Tartaruga Marinha',
+        'Estrela do Mar'
+      ],
+      'correctAnswer': 'Tartaruga Marinha'
+    }
+  ];
+
+  void nextQuestion() {
+    selectedAnswer = null;
+    isCorret = null;
+
+    if (currentQuestionIndex < questions.length - 1) {
+      setState(() {
+        currentQuestionIndex++;
+      });
+    } else {
+      setState(() {
+        quizConcluido = true;
+        Future.delayed(Duration(seconds: 5), () {
+          Navigator.of(context).push(MaterialPageRoute(builder:(_)=> new HomePage()));
+        });
+      });
+    }
+  }
+
+  void handleAnswer(String answer) {
+    setState(() {
+      selectedAnswer = answer;
+      isCorret = answer == questions[currentQuestionIndex]['correctAnswer'];
+    });
+
+    Future.delayed(Duration(seconds: 2), () {
+      nextQuestion();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (quizConcluido) {
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Card(
+            color: Colors.blue[50],
+            elevation: 4,
+            child: Column(
+              children: [
+                Image.asset('assets/gifParabens.gif', width: 400, height: 300),
+                Text('Você finalizou o quiz!\nVocê acertou $questoesAcertadas questões!\nRetornando para a página inicial...',
+                  style: GoogleFonts.roboto(fontSize: 32, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ],
+      );
+    }
+
+    var currentQuestion = questions[currentQuestionIndex];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Quiz marítimo!', style: GoogleFonts.roboto(fontWeight: FontWeight.bold)),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(18),
+            color: Colors.deepPurple[50],
+            width: double.infinity,
+            height: 400,
+            child: Center(
+              child: Text(
+                currentQuestion['question'],
+                style: GoogleFonts.roboto(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+            ),
+          ),
+          Wrap(
+            children: currentQuestion['answers'].map<Widget>((resposta) {
+              bool isSelected = selectedAnswer == resposta;
+              Color? buttonColor;
+              if (isSelected) {
+                buttonColor = isCorret! ? Colors.green : Colors.red;
+                isCorret! ? questoesAcertadas++ : null;
+              }
+
+              return meuBtn(resposta, () => handleAnswer(resposta), buttonColor);
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget meuBtn(String resposta, VoidCallback onPressed, Color? color) => Container(
+      margin: const EdgeInsets.all(16),
+      width: 160,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+        ),
+        child: Text(resposta),
+      ),
+    );
---
+++ lib/quizTerrestre.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class QuizTerrestre extends StatefulWidget {
+  const QuizTerrestre({super.key});
+
+  @override
+  State<QuizTerrestre> createState() => _QuizState();
+}
+
+class _QuizState extends State<QuizTerrestre> {
+  int currentQuestionIndex = 0; // Índice da pergunta atual
+  String? selectedAnswer;
+  bool? isCorret;
+  final List<Map<String, dynamic>> questions = [
+    {
+      'question':
+          'Qual destes animais terrestres é conhecido por sua tromba longa e flexível?',
+      'answers': ['Leão', 'Elefante', 'Girafa', 'Zebra'],
+      'correctAnswer': 'Elefante'
+    },
+    {
+      'question':
+          'Qual destes animais terrestres é o mamífero mais rápido do mundo?',
+      'answers': ['Leopardo', 'Guepardo', 'Leão', 'Gorila'],
+      'correctAnswer': 'Guepardo'
+    },
+    {
+      'question':
+        'Qual destes animais terrestres é um marsupial e é encontrado principalmente na Austrália?',
+      'answers': ['Leão', 'Canguru', 'Lobo', 'Tigre'],
+      'correctAnswer': 'Canguru'
+    }
+    //Adicione mais perguntas aqui
+  ];
+
+  void nextQuestion() {
+    selectedAnswer = null;
+    isCorret = null;
+    if (currentQuestionIndex < questions.length - 1) {
+      setState(() {
+        currentQuestionIndex++;
+      });
+    } else {
+      //Fim do Quiz, fazer algo aqui
+      
+    }
+  }
+
+  void handleAnswer(String answer) {
+    setState(() {
+      selectedAnswer = answer; 
+      isCorret = answer == questions[currentQuestionIndex]['correctAnswer'];  
+    });
+
+    Future.delayed(Duration(seconds: 2), () {
+      nextQuestion();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var currentQuestion = questions[currentQuestionIndex];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Quiz marítimo!', style: GoogleFonts.roboto(fontWeight: FontWeight.bold))
+        ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(18),
+            color: Colors.deepPurple[50],
+            width: double.infinity,
+            height: 400,
+            child: Center (
+              child: Text(currentQuestion['question'],
+              style: GoogleFonts.roboto(fontWeight: FontWeight.bold, fontSize: 18), ),
+            ),
+          ),
+          Wrap(
+            children: currentQuestion['answers'].map<Widget>((resposta) {
+              bool isSelected = selectedAnswer == resposta;
+              Color? buttonColor;
+              if (isSelected) {
+                buttonColor = isCorret! ? Colors.green : Colors.red;
+              }
+
+              return meuBtn(
+                resposta, () => handleAnswer(resposta), buttonColor);
+            }).toList(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+Widget meuBtn(String resposta, VoidCallback onPressed, Color? color) => 
+  Container(
+    margin: const EdgeInsets.all(16),
+    width: 160,
+    child: ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+      ),
+      child: Text(resposta),
+    ),
+  );
